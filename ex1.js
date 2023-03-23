993dcb6f0a9ea87c171a797dfe48bc74c8adbd03
Jules257 <julesnzejimana@gmail.com>
2023-03-23 22:16:29 +0100

    some changes

--- ex1.js
+++ ex1.js
@@ -22,12 +22,26 @@ return xmlHttp;
 
 function process(){
     if(xmlHttp.readyState == 0 || xmlHttp.readyState == 4){
-        nam = encodeURIComponent(document.getElementById('name').value);
-        xmlHttp.open('GET','ex1.php?name='+ nam, true);
+        name = encodeURIComponent(document.getElementById('name').value);
+        xmlHttp.open('GET','ex1.php?name='+ name, true);
         xmlHttp.onreadystatechange = handleResponse;
         xmlHttp.send(null);
 
     }else{
         setTimeout('process()',1000)
+    }
+}
+function   handleResponse(){
+    if(xmlHttp.readyState == 4){
+        if(xmlHttp.status == 200){
+         reponse = xmlHttp.responseXML;
+         xmlRoot = reponse.documentElement;
+         message = xmlRoot.firstChild.data;
+         document.getElementById('answer').innerHTML = '<span style="color: red;">'
+                                                       + message +  '</span>';
+         setTimeout('process()', 1000);                                                                          
+        } else{
+            alert('erreur au niveau de la lecture');
+        }
     }
 }
