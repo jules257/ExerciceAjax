d2a17b9c4953518a297b1734eaccfa83f28ccd9f
Jules257 <julesnzejimana@gmail.com>
2023-03-23 21:47:59 +0100

    some changes

---
+++ ex1.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-Type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8' atandalone='yea'";
+echo '<response>';
+$name =$_GET['name'];
+$maitres = array('HONORE','BELYNDA','MARIE');
+if(in_array($name,$maitres)){
+    echo 'salut'.htmlentities($name);
+}else if(trim($name) == ''){
+echo 'coucou etrager!! quel est votre nom??';
+}else{
+ echo 'OMG'.htmlentities($name).'.je ne te connais pas!!';   
+}
+echo '<response>';
+
+?>
